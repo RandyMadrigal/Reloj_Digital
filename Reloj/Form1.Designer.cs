381e419e5eb2c953a6183d11aff11f4fe70c6c62
Randy Madrigal <randymadrigalx12@gmail.com>
2021-07-05 23:15:45 -0400

    Agregando reloj

---
+++ Reloj/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace Reloj
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelReloj = new System.Windows.Forms.Label();
+            this.LabelFecha = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelReloj
+            // 
+            this.labelReloj.Font = new System.Drawing.Font("Arial Rounded MT Bold", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelReloj.ForeColor = System.Drawing.Color.SpringGreen;
+            this.labelReloj.Location = new System.Drawing.Point(160, 58);
+            this.labelReloj.Name = "labelReloj";
+            this.labelReloj.Size = new System.Drawing.Size(253, 48);
+            this.labelReloj.TabIndex = 0;
+            // 
+            // LabelFecha
+            // 
+            this.LabelFecha.Font = new System.Drawing.Font("Arial Rounded MT Bold", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelFecha.ForeColor = System.Drawing.Color.SpringGreen;
+            this.LabelFecha.Location = new System.Drawing.Point(160, 117);
+            this.LabelFecha.Name = "LabelFecha";
+            this.LabelFecha.Size = new System.Drawing.Size(366, 40);
+            this.LabelFecha.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.DimGray;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(141, 126);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(36)))), ((int)(((byte)(36)))));
+            this.ClientSize = new System.Drawing.Size(560, 224);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.LabelFecha);
+            this.Controls.Add(this.labelReloj);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Reloj";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelReloj;
+        private System.Windows.Forms.Label LabelFecha;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Reloj/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Drawing;
+
+using System.Windows.Forms;
+
+namespace Reloj
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Icon = new Icon(@"Icon\reloj.ico");
+            pictureBox1.Image = Image.FromFile(@"Icon\clock.gif");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            labelReloj.Text = DateTime.Now.ToLongTimeString();
+            LabelFecha.Text = DateTime.Now.ToLongDateString();
+        }
+
+    }
+}
